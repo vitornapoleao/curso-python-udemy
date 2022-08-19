538082d96d4060d8f49cad2178b4c57172bf6526
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-19 12:21:38 -0300

    Create variaveis2.py

---
+++ variaveis2.py
@@ -0,0 +1,7 @@
+#crie duas variaveis com valores numericos
+# atribua a soma das variaveis a uma outra variavel
+#imprima esse valor
+numero = 6
+numero2 = 6
+soma = (numero + numero2)
+print (soma)
