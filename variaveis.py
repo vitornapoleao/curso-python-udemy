36144be19ce51ce6ad8c4196cc3b69fe671fe4d8
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-19 11:56:55 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+#exercicio de numero três 
+#criar uma variavel que vai armazenar algum texto
+#e exibir o texto na tela
+# exibir o tipo da variavel
+bentinho = "dom_casmurro"
+print(bentinho)
+print(type(bentinho))
+print("será que Capitu traiu bentinho? seria Dom. Casmurro corno?")
