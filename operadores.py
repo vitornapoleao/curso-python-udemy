9233a8f0b4c70e2394f6d9f3d8443c0505a6e402
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-19 13:23:24 -0300

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#Exercicio 5
+#Crie variaveis exemplo: a = 5, b = 3, c = 10, d = 2;
+#Realize as seguintes comparações;
+#a > b; c ==a; d!=a; a <= c; b >c ; a != c;
+#d >= a; d != b; d < c; b >= a
+a = 3
+b = 4
+c = 5 
+d = 6
+print(a > b)
+print(c == a)
+print(d != a)
+print(a <= c)
+print(b > c)
+print(a != c)
+print(d >=a )
+print(d != b)
+print(d < c)
+print(b >= a)
