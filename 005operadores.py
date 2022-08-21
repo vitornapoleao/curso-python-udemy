afcd4e122ec0064dc11afb79e735ac36d1cb31e5
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-21 11:58:02 -0300

    Create 005operadores.py

---
+++ 005operadores.py
@@ -0,0 +1,18 @@
+#EXERCIO 6
+#crie uma variavel x = true, y = false  e z = false;
+#faça as comparações;
+#x and y; not x; x and x
+#y and y; y or x; y and z; not y; x and z;
+
+x = True 
+y = False
+z = False
+
+print(x and y)
+print(not x )
+print(x and x)
+print(y and y)
+print(y or x )
+print(y and z )
+print(not y)
+print(x and z)
