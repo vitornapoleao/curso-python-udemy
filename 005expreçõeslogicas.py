9298daca56addc1ab34480f7692dee192c0ceeb5
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-22 10:45:06 -0300

    Create 005expreçõeslogicas.py

---
+++ 005expreçõeslogicas.py
@@ -0,0 +1,12 @@
+#escreva uma expressão numerica que verifica se o aluno atingiu
+#a media escolar que é 7
+#crie tres variaveis com notas distintas;
+#exiba o resultado com print
+media = 7 
+nota_a = 8
+nota_b = 6
+nota_c = 5
+media_aluno = (nota_a + nota_b + nota_c) / 3
+
+print(media_aluno >= media)
+
