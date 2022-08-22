fb47decf4a68c51444d81d48dc17b004c240d093
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-22 11:05:33 -0300

    Create 007expressoes.py

---
+++ 007expressoes.py
@@ -0,0 +1,11 @@
+#crie algumas variaveis com o valor de velocidade;
+#crie tambem uma variavel limite de 80;
+#imprima expressoes logicas que verificam se as velocidades
+# maiores ou iguais a 80
+carro_y_velocidade = 45
+carro_x_velocidade = 60
+carro_z_velocidade = 99
+limite_de_velocidade = 80
+print(carro_y_velocidade > limite_de_velocidade)
+print(carro_x_velocidade > limite_de_velocidade)
+print(carro_z_velocidade > limite_de_velocidade )
