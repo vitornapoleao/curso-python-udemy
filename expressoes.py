8fd254f2b995c3c9e5515e9ec1e8df57888a836d
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-22 10:51:16 -0300

    Create expressoes.py

---
+++ expressoes.py
@@ -0,0 +1,9 @@
+#crie uma variavel salario com um valor de sua escolha
+#crie uma variavel aumento com um valor tambem de sua escolha 
+#atribua a variaveç novo_salario o valor do salario com o
+#aumento 
+#exiba o resultado na tela
+salario = 650
+aumento = 60
+salario_novo = salario + aumento
+print(salario_novo)
