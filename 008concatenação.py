23dd3122cdc3b7c4c32977e99bec09bfd54c59b3
vitornapoleao <109608876+vitornapoleao@users.noreply.github.com>
2022-08-25 11:24:46 -0300

    Update and rename 007concatenação.py to 008concatenação.py

---
+++ 008concatenação.py
@@ -0,0 +1,8 @@
+
+#Crie uma variavel com uma saudação e outra com o seu nome
+#imprima a saudação completa por meio de concatenação
+#ex: Olá, sr. vitor
+pronome = "Ola"
+nome = " Sr. Vitor "
+
+print(pronome + nome)
